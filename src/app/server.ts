b7300e6aedfb4fb217ed435e35b84f4f037ca37f
sabbir990 <muhammadsabbirkhanalif@gmail.com>
2025-10-27 08:07:00 +0600

    Added cors configuration

--- src/app/server.ts
+++ src/app/server.ts
@@ -3,9 +3,15 @@ import dotenv from "dotenv";
 import {Server} from "http";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 dotenv.config();
 app.use(cookieParser());
+
+app.use(cors({
+    origin : process.env.CLIENT_URL || "http://localhost:5173/",
+    credentials : true
+}))
 
 let server : Server;
 let PORT = process.env.PORT || 5000;
