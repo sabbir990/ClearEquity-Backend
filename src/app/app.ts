eab400a143a8fc032d82bfca32395ae9e319e99a
sabbir990 <muhammadsabbirkhanalif@gmail.com>
2025-10-27 17:06:56 +0600

    Added a new origin in cors config

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 const app : Application = express();
 
 app.use(cors({
-    origin : ["http://localhost:5173", "http://localhost:5173"],
+    origin : ["http://localhost:5173", "http://localhost:5174"],
     credentials : true
 }))
 
