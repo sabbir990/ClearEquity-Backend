74ec5ef018ff002a7c32dadee2dc4776ce35886b
sabbir990 <muhammadsabbirkhanalif@gmail.com>
2025-12-01 16:38:25 +0600

    Configured cloudinary setup properly phase 1

--- src/app/controllers/cloudinary.controller.ts
+++ src/app/controllers/cloudinary.controller.ts
@@ -3,27 +3,42 @@ import cloudinary from "../utils/cloudinarySetup";
 import multer from "multer";
 
 export const cloudinaryRouter = express.Router();
-const upload = multer({dest: "uploads/"});
 
-cloudinaryRouter.post("/upload", upload.single("file"), async(req: Request, res: Response) => {
-    try{
-        if(!req.file){
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+cloudinaryRouter.get("/", async (req: Request, res: Response) => {
+    res.send("This is cloudinary route for the server!")
+})
+
+cloudinaryRouter.post("/upload", upload.single("file"), async (req: Request, res: Response) => {
+    try {
+        if (!req.file) {
             return res.json({
                 message: "File not found!"
             })
         }
-        const uploadResult = await cloudinary.uploader.upload(req.file.path, {
-            resource_type: "auto",
-            folder: "my_files"
+
+        const uploadResult = await new Promise((resolve, reject) => {
+            const stream = cloudinary.uploader.upload_stream({
+                folder: "my_files",
+                resource_type: "auto"
+            },
+                (error, result) => {
+                    if (error) reject(error);
+                    else resolve(result);
+                }
+            )
+
+            stream.end(req.file?.buffer);
+
         })
 
         res.json({
-            url: uploadResult.secure_url,
-            public_id: uploadResult.public_id,
-            type: uploadResult.resource_type
+            uploadResult
         })
-    }catch(err){
-        res.status(500).json({message: "Error uploading file!"})
+    } catch (err) {
+        res.status(500).json({ message: "Error uploading file!" })
     }
 })
 
